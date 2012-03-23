48198c859eae6b619f774734189f8346aca36284
Lim Chee Aun <cheeaun@gmail.com>
2012-03-24 02:45:09 +0800

    Try request's Forever agent.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var journey = require('journey'),
+	request = require('request'),
 	jsdom = require('jsdom'),
 	fs = require('fs'),
 	jquery = fs.readFileSync("./jquery.min.js").toString(),
@@ -59,62 +60,71 @@ router.map(function () {
 				if (callback) result = callback + '(' + result + ')';
 				res.sendBody(result);
 			} else {
-				jsdom.env({
-					html: ROOT_URL + (path!='news' ? path : ''),
-					src: [jquery],
-					done: function(err, window){
-						if (err){
-							errorRespond(res, err, callback);
-							return;
-						}
-						var $ = window.$;
-						var posts = [],
-							rows = $('td table:has(td.title) tr:has(td)');
-						for (var i=0, l=rows.length; i<l; i+=2){
-							var row1 = $(rows[i]),
-								row2 = $(rows[i+1]);
-							if (!row2.length) break;
-							var voteLink = row1.find('td a[id^=up]'),
-								id = (voteLink.length ? (voteLink.attr('id').match(/\d+/) || [])[0] : null),
-								cell1 = row1.find('td.title:has(a)'),
-								link = cell1.find('a'),
-								title = link.text().trim(),
-								url = link.attr('href'),
-								domain = (cell1.find('.comhead').text().match(/\(\s?([^()]+)\s?\)/i) || [,null])[1],
-								cell2 = row2.find('td.subtext'),
-								points = parseInt(cell2.find('span[id^=score]').text(), 10),
-								userLink = cell2.find('a[href^=user]'),
-								user = userLink.text() || null,
-								timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
-								commentsCount = parseInt(cell2.find('a[href^=item]').text(), 10) || 0,
-								type = 'link';
-							if (url.match(/^item/i)) type = 'ask';
-							if (!user){ // No users post this = job ads
-								type = 'job';
-								id = (url.match(/\d+/) || [])[0];
-								timeAgo = cell2.text().trim();
-							}
-							posts.push({
-								id: id,
-								title: title,
-								url: url,
-								domain: domain,
-								points: points,
-								user: user,
-								time_ago: timeAgo,
-								comments_count: commentsCount,
-								type: type
-							});
-						}
-
-						var postsJSON = JSON.stringify(posts);
-						redis.set(path, postsJSON);
-						redis.expire(path, CACHE_EXP);
-						if (callback) postsJSON = callback + '(' + postsJSON + ')';
-						res.sendBody(postsJSON);
-
-						window.close();
+				request({
+					url: ROOT_URL + (path!='news' ? path : ''),
+					forever: true
+				}, function(e, r, body){
+					if (e || r.statusCode != 200){
+						errorRespond(res, e, callback);
+						return;
 					}
+					jsdom.env({
+						html: body,
+						src: [jquery],
+						done: function(err, window){
+							if (err){
+								errorRespond(res, err, callback);
+								return;
+							}
+							var $ = window.$;
+							var posts = [],
+								rows = $('td table:has(td.title) tr:has(td)');
+							for (var i=0, l=rows.length; i<l; i+=2){
+								var row1 = $(rows[i]),
+									row2 = $(rows[i+1]);
+								if (!row2.length) break;
+								var voteLink = row1.find('td a[id^=up]'),
+									id = (voteLink.length ? (voteLink.attr('id').match(/\d+/) || [])[0] : null),
+									cell1 = row1.find('td.title:has(a)'),
+									link = cell1.find('a'),
+									title = link.text().trim(),
+									url = link.attr('href'),
+									domain = (cell1.find('.comhead').text().match(/\(\s?([^()]+)\s?\)/i) || [,null])[1],
+									cell2 = row2.find('td.subtext'),
+									points = parseInt(cell2.find('span[id^=score]').text(), 10),
+									userLink = cell2.find('a[href^=user]'),
+									user = userLink.text() || null,
+									timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
+									commentsCount = parseInt(cell2.find('a[href^=item]').text(), 10) || 0,
+									type = 'link';
+								if (url.match(/^item/i)) type = 'ask';
+								if (!user){ // No users post this = job ads
+									type = 'job';
+									id = (url.match(/\d+/) || [])[0];
+									timeAgo = cell2.text().trim();
+								}
+								posts.push({
+									id: id,
+									title: title,
+									url: url,
+									domain: domain,
+									points: points,
+									user: user,
+									time_ago: timeAgo,
+									comments_count: commentsCount,
+									type: type
+								});
+							}
+
+							var postsJSON = JSON.stringify(posts);
+							redis.set(path, postsJSON);
+							redis.expire(path, CACHE_EXP);
+							if (callback) postsJSON = callback + '(' + postsJSON + ')';
+							res.sendBody(postsJSON);
+
+							window.close();
+						}
+					});
 				});
 			}
 		});
@@ -127,170 +137,188 @@ router.map(function () {
 				if (callback) result = callback + '(' + result + ')';
 				res.sendBody(result);
 			} else {
-				jsdom.env({
-					html: ROOT_URL + 'item?id=' + postID,
-					src: [jquery],
-					done: function(err, window){
-						if (err){
-							errorRespond(res, err, callback);
-							return;
-						}
-						var $ = window.$;
-						var table1 = $('td table:has(td.title,textarea)'),
-							voteLink = table1.find('td a[id^=up]'),
-							id = (voteLink.length ? (voteLink.attr('id').match(/\d+/) || [])[0] : null),
-							cell1 = table1.find('td.title:has(a)'),
-							link, title, url, domain, points, user, timeAgo, commentsCount, content,
-							poll = null,
-							type = 'link';
-						if (cell1.length){
-							link = cell1.find('a');
-							title = link.text().trim();
-							url = link.attr('href');
-							domain = (cell1.find('.comhead').text().match(/\(\s?([^()]+)\s?\)/i) || [,null])[1];
-							var cell2 = table1.find('td.subtext');
-							points = parseInt(cell2.find('span[id^=score]').text(), 10);
-							var userLink = cell2.find('a[href^=user]');
-							user = userLink.text() || null;
-							timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '';
-							commentsCount = parseInt(cell2.find('a[href^=item]').text(), 10) || 0;
-							var questionCell = cell2.parent('tr').nextAll('tr:has(td):first').find('td:not(:empty):not(:has(textarea))');
-							content = questionCell.length ? cleanContent(questionCell.html()) : null;
-							var pollCell = questionCell.parent('tr').nextAll('tr:has(td):first').find('td:not(:empty):not(:has(textarea)):has(td.comment)');
-							if (pollCell.length){
-								poll = [];
-								pollCell.find('td.comment').each(function(){
-									var el = $(this);
-									poll.push({
-										item: el.text().trim(),
-										points: parseInt(el.parent('tr').next('tr').find('.comhead span').text(), 10)
-									});
-								});
+				request({
+					url: ROOT_URL + 'item?id=' + postID,
+					forever: true
+				}, function(e, r, body){
+					if (e || r.statusCode != 200){
+						errorRespond(res, e, callback);
+						return;
+					}
+					jsdom.env({
+						html: body,
+						src: [jquery],
+						done: function(err, window){
+							if (err){
+								errorRespond(res, err, callback);
+								return;
 							}
-							if (url.match(/^item/i)) type = 'ask';
-							if (!user){ // No users post this = job ads
-								type = 'job';
-								id = (url.match(/\d+/) || [])[0];
-								timeAgo = cell2.text().trim();
-							}
-						} else {
-							var cell = table1.find('td.default');
-							if (cell.length){
-								var userLink = cell.find('a[href^=user]');
-								user = userLink.text(),
-								timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
-								id = (cell.find('a[href^=item]').attr('href').match(/\d+/) || [])[0],
-								content = cleanContent(table1.find('.comment').html());
-								type = 'comment';
-							}
-						}
-						var post = {
-								id: id,
-								title: title,
-								url: url,
-								domain: domain,
-								points: points,
-								user: user,
-								time_ago: timeAgo,
-								comments_count: commentsCount,
-								content: content,
-								poll: poll,
-								type: type,
-								comments: []
-							},
-							table2 = table1.nextAll('table:first');
-
-						var done = function(window){
-							var postJSON = JSON.stringify(post);
-							redis.set('post' + postID, postJSON);
-							redis.expire('post' + postID, CACHE_EXP);
-							if (callback) postJSON = callback + '(' + postJSON + ')';
-							res.sendBody(postJSON);
-
-							window.close();
-						};
-						
-						// If there are comments for a post
-						if (table2.length){
-							var commentRows = table2.find('tr table');
-							var processComments = function(rows){
-								var comments = [];
-
-								// Create flat array of comments
-								for (var i=0, l=rows.length; i<l; i++){
-									var row = $(rows[i]),
-										comment = {},
-										level = parseInt(row.find('img[src*="s.gif"]').attr('width'), 10) / 40,
-										metadata = row.find('.comhead:has(a)'),
-										user = null,
-										timeAgo = '',
-										id = '',
-										content = '[deleted]';
-									if (metadata.length){
-										var userLink = metadata.find('a[href^=user]');
-										user = userLink.text(),
-										timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
-										id = (metadata.find('a[href^=item]').attr('href').match(/\d+/) || [])[0],
-										content = cleanContent(row.find('.comment').html());
-									}
-									comments.push({
-										id: id,
-										level: level,
-										user: user,
-										time_ago: timeAgo,
-										content: content,
-										comments: []
+							var $ = window.$;
+							var table1 = $('td table:has(td.title,textarea)'),
+								voteLink = table1.find('td a[id^=up]'),
+								id = (voteLink.length ? (voteLink.attr('id').match(/\d+/) || [])[0] : null),
+								cell1 = table1.find('td.title:has(a)'),
+								link, title, url, domain, points, user, timeAgo, commentsCount, content,
+								poll = null,
+								type = 'link';
+							if (cell1.length){
+								link = cell1.find('a');
+								title = link.text().trim();
+								url = link.attr('href');
+								domain = (cell1.find('.comhead').text().match(/\(\s?([^()]+)\s?\)/i) || [,null])[1];
+								var cell2 = table1.find('td.subtext');
+								points = parseInt(cell2.find('span[id^=score]').text(), 10);
+								var userLink = cell2.find('a[href^=user]');
+								user = userLink.text() || null;
+								timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '';
+								commentsCount = parseInt(cell2.find('a[href^=item]').text(), 10) || 0;
+								var questionCell = cell2.parent('tr').nextAll('tr:has(td):first').find('td:not(:empty):not(:has(textarea))');
+								content = questionCell.length ? cleanContent(questionCell.html()) : null;
+								var pollCell = questionCell.parent('tr').nextAll('tr:has(td):first').find('td:not(:empty):not(:has(textarea)):has(td.comment)');
+								if (pollCell.length){
+									poll = [];
+									pollCell.find('td.comment').each(function(){
+										var el = $(this);
+										poll.push({
+											item: el.text().trim(),
+											points: parseInt(el.parent('tr').next('tr').find('.comhead span').text(), 10)
+										});
 									});
 								}
-								// Comments are not nested yet, this 2nd loop will nest 'em up
-								for (var i=0, l=comments.length; i<l; i++){
-									var comment = comments[i],
-										level = comment.level;
-									if (level > 0){
-										var index = i, parentComment;
-										do {
-											parentComment = comments[--index];
-										} while (parentComment.level >= level);
-										parentComment.comments.push(comment);
-									}
+								if (url.match(/^item/i)) type = 'ask';
+								if (!user){ // No users post this = job ads
+									type = 'job';
+									id = (url.match(/\d+/) || [])[0];
+									timeAgo = cell2.text().trim();
 								}
-								// After that, remove the non-nested ones
-								comments = comments.filter(function(comment){
-									return (comment.level == 0);
-								});
-								return comments;
+							} else {
+								var cell = table1.find('td.default');
+								if (cell.length){
+									var userLink = cell.find('a[href^=user]');
+									user = userLink.text(),
+									timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
+									id = (cell.find('a[href^=item]').attr('href').match(/\d+/) || [])[0],
+									content = cleanContent(table1.find('.comment').html());
+									type = 'comment';
+								}
+							}
+							var post = {
+									id: id,
+									title: title,
+									url: url,
+									domain: domain,
+									points: points,
+									user: user,
+									time_ago: timeAgo,
+									comments_count: commentsCount,
+									content: content,
+									poll: poll,
+									type: type,
+									comments: []
+								},
+								table2 = table1.nextAll('table:first');
+
+							var done = function(window){
+								var postJSON = JSON.stringify(post);
+								redis.set('post' + postID, postJSON);
+								redis.expire('post' + postID, CACHE_EXP);
+								if (callback) postJSON = callback + '(' + postJSON + ')';
+								res.sendBody(postJSON);
+
+								window.close();
 							};
 							
-							post.comments = processComments(commentRows);
+							// If there are comments for a post
+							if (table2.length){
+								var commentRows = table2.find('tr table');
+								var processComments = function(rows){
+									var comments = [];
 
-							// Check for 'More' comments (Rare case)
-							// Yet another request here, getting the 2nd page of the comments
-							// Not sure if there's a 3rd page, but who cares?
-							var more = $('td.title a[href^="/x?"]');
-							if (more.length){
-								var url = ROOT_URL + more.attr('href').replace(/^\//, '');
-								window.close();
-								jsdom.env({
-									html: url,
-									src: [jquery],
-									done: function(err, window1){
-										if (err){
-											errorRespond(res, err, callback);
+									// Create flat array of comments
+									for (var i=0, l=rows.length; i<l; i++){
+										var row = $(rows[i]),
+											comment = {},
+											level = parseInt(row.find('img[src*="s.gif"]').attr('width'), 10) / 40,
+											metadata = row.find('.comhead:has(a)'),
+											user = null,
+											timeAgo = '',
+											id = '',
+											content = '[deleted]';
+										if (metadata.length){
+											var userLink = metadata.find('a[href^=user]');
+											user = userLink.text(),
+											timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
+											id = (metadata.find('a[href^=item]').attr('href').match(/\d+/) || [])[0],
+											content = cleanContent(row.find('.comment').html());
+										}
+										comments.push({
+											id: id,
+											level: level,
+											user: user,
+											time_ago: timeAgo,
+											content: content,
+											comments: []
+										});
+									}
+									// Comments are not nested yet, this 2nd loop will nest 'em up
+									for (var i=0, l=comments.length; i<l; i++){
+										var comment = comments[i],
+											level = comment.level;
+										if (level > 0){
+											var index = i, parentComment;
+											do {
+												parentComment = comments[--index];
+											} while (parentComment.level >= level);
+											parentComment.comments.push(comment);
+										}
+									}
+									// After that, remove the non-nested ones
+									comments = comments.filter(function(comment){
+										return (comment.level == 0);
+									});
+									return comments;
+								};
+								
+								post.comments = processComments(commentRows);
+
+								// Check for 'More' comments (Rare case)
+								// Yet another request here, getting the 2nd page of the comments
+								// Not sure if there's a 3rd page, but who cares?
+								var more = $('td.title a[href^="/x?"]');
+								if (more.length){
+									var url = ROOT_URL + more.attr('href').replace(/^\//, '');
+									window.close();
+									request({
+										url: url,
+										forever: true
+									}, function(e, r, body){
+										if (e || r.statusCode != 200){
+											errorRespond(res, e, callback);
 											return;
 										}
-										var $ = window1.$;
-										var commentRows = $('table:not(:has(table)):has(a[id^=up])');
-										post.comments = post.comments.concat(processComments(commentRows));
-										done(window1);
-									}
-								});
+										jsdom.env({
+											html: body,
+											src: [jquery],
+											done: function(err, window1){
+												if (err){
+													errorRespond(res, err, callback);
+													return;
+												}
+												var $ = window1.$;
+												var commentRows = $('table:not(:has(table)):has(a[id^=up])');
+												post.comments = post.comments.concat(processComments(commentRows));
+												done(window1);
+											}
+										});
+									});
+								} else {
+									done(window);
+								}
 							} else {
 								done(window);
 							}
-						} else {
-							done(window);
 						}
-					}
+					});
 				});
 			}
 		});
