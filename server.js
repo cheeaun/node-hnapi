877df265bc49fc4e6c3be7a0640e0daa10bed525
Lim Chee Aun <cheeaun@gmail.com>
2013-05-19 00:22:32 +0800

    Extract out the dom parsing stuff into a module

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ if (nodeflyKey){
 var express = require('express');
 var cors = require('cors');
 var https = require('https');
-var domino = require('domino');
-var fs = require('fs');
-var jquery = fs.readFileSync(__dirname + '/jquery.min.js').toString();
+var hndom = require('./lib/hndom.js');
 var zlib = require('zlib');
 var redis = require('redis');
 var memory = require('memory-cache');
@@ -171,15 +169,6 @@ var errorRespond = function(res, error){
 	});
 };
 
-var cleanContent = function(html){
-	// yea yea regex to clean HTML is lame yada yada
-	html = html.replace(/">-+<\/font/ig, '"></font'); // remove weird invisible dashes at the end of comments
-	html = html.replace(/<\/?font[^<>]*>/ig, ''); // remove font tags
-	html = html.replace(/<\/p>/ig, ''); // remove trailing </p>s
-	if (!html.match(/^<p>/i)) html = '<p>' + html; // prepend <p>
-	return html;
-};
-
 var request = function(path, fn){
 	var start;
 	var req = REQUESTS[path];
@@ -243,52 +232,9 @@ app.get(/^\/(news|news2|newest|ask|best|active|noobstories)$/, function(req, res
 					errorRespond(res, err);
 					return;
 				}
-				var window = domino.createWindow(body);
-				window._run(jquery);
-				var $ = window.$;
-
-				var posts = [],
-					rows = $('td table:has(td.title) tr');
-				rows = rows.has('td');
-				for (var i=0, l=rows.length; i<l; i+=2){
-					var row1 = $(rows[i]),
-						row2 = $(rows[i+1]);
-					if (!row2.length) break;
-					var voteLink = row1.find('td a[id^=up]'),
-						id = (voteLink.length ? (voteLink.attr('id').match(/\d+/) || [])[0] : null),
-						cell1 = row1.find('td.title').has('a'),
-						link = cell1.find('a:first'),
-						title = link.text().trim(),
-						url = link.attr('href'),
-						domain = (cell1.find('.comhead').text().match(/\(\s?([^()]+)\s?\)/i) || [,null])[1],
-						cell2 = row2.find('td.subtext'),
-						points = parseInt(cell2.find('span[id^=score]').text(), 10),
-						userLink = cell2.find('a[href^=user]'),
-						user = userLink.text() || null,
-						timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
-						commentsCount = parseInt(cell2.find('a[href^=item]').text(), 10) || 0,
-						type = 'link';
-					if (url.match(/^item/i)) type = 'ask';
-					if (!user){ // No users post this = job ads
-						type = 'job';
-						id = (url.match(/\d+/) || [])[0];
-						timeAgo = cell2.text().trim();
-					}
-					posts.push({
-						id: id,
-						title: title,
-						url: url,
-						domain: domain,
-						points: points,
-						user: user,
-						time_ago: timeAgo,
-						comments_count: commentsCount,
-						type: type
-					});
-				}
-
-				cache.set(cacheKey, posts, CACHE_EXP);
-				res.jsonp(posts);
+				var data = hndom.stories(body);
+				cache.set(cacheKey, data, CACHE_EXP);
+				res.jsonp(data);
 			});
 
 			// If 'news' expired, 'news2' should expire too
@@ -296,70 +242,6 @@ app.get(/^\/(news|news2|newest|ask|best|active|noobstories)$/, function(req, res
 		}
 	});
 });
-
-var processComments = function(rows, $){
-	var comments = [];
-
-	// Create flat array of comments
-	for (var i=0, l=rows.length; i<l; i++){
-		var row = $(rows[i]),
-			comment = {},
-			level = 0,
-			levelRow = row.find('img[src*="s.gif"]'),
-			metadata = row.find('.comhead').has('a'),
-			user = '',
-			timeAgo = '',
-			id = '',
-			content = '[deleted]';
-		if (levelRow.length){
-			level = parseInt((levelRow).attr('width'), 10) / 40;
-		}
-		if (metadata.length){
-			var userLink = metadata.find('a[href^=user]');
-			user = userLink.text();
-			timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '';
-			id = (metadata.find('a[href^=item]').attr('href').match(/\d+/) || [])[0];
-			var commentEl = row.find('.comment');
-			var replyLink = commentEl.find('a[href^=reply]');
-			// Sometimes the markup becomes nice, and 'reply' link is not part of the comments
-			if (replyLink.length){
-				// Remove 'reply' link
-				if (replyLink.parent('u').length){
-					replyLink.parent().remove();
-				} else {
-					replyLink.remove();
-				}
-			}
-			content = cleanContent(commentEl.html());
-		}
-		comments.push({
-			id: id,
-			level: level,
-			user: user,
-			time_ago: timeAgo,
-			content: content,
-			comments: []
-		});
-	}
-
-	// Comments are not nested yet, this 2nd loop will nest 'em up
-	for (var i=0, l=comments.length; i<l; i++){
-		var comment = comments[i],
-			level = comment.level;
-		if (level > 0){
-			var index = i, parentComment;
-			do {
-				parentComment = comments[--index];
-			} while (parentComment.level >= level);
-			parentComment.comments.push(comment);
-		}
-	}
-	// After that, remove the non-nested ones
-	comments = comments.filter(function(comment){
-		return (comment.level == 0);
-	});
-	return comments;
-};
 
 app.get(/^\/item\/(\d+)$/, function(req, res){
 	var postID = req.params[0];
@@ -374,102 +256,9 @@ app.get(/^\/item\/(\d+)$/, function(req, res){
 					errorRespond(res, err);
 					return;
 				}
-				var window = domino.createWindow(body);
-				window._run(jquery);
-				var $ = window.$;
-
-				var table1 = $('td table:has(td.title,textarea)'),
-					voteLink = table1.find('td a[id^=up]'),
-					id = (voteLink.length ? (voteLink.attr('id').match(/\d+/) || [])[0] : null),
-					cell1 = table1.find('td.title').has('a'),
-					link, title, url, domain, points, user, timeAgo, commentsCount,
-					content = null,
-					poll = null,
-					type = 'link';
-				if (cell1.length){
-					link = cell1.find('a').first();
-					title = link.text().trim();
-					url = link.attr('href');
-					domain = (cell1.find('.comhead').text().match(/\(\s?([^()]+)\s?\)/i) || [,null])[1];
-					var cell2 = table1.find('td.subtext');
-					points = parseInt(cell2.find('span[id^=score]').text(), 10);
-					var userLink = cell2.find('a[href^=user]');
-					user = userLink.text() || null;
-					timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '';
-					commentsCount = parseInt(cell2.find('a[href^=item]').text(), 10) || 0;
-					var nextContentRows = cell2.parent('tr').nextAll('tr:not(:empty):not(:has(textarea))');
-					var questionCell = nextContentRows.eq(0).children('td:not(:empty)');
-					var pollCell;
-					// The content could be question+poll, question or poll.
-					if (questionCell.length && !questionCell.find('td.comment').length){
-						content = cleanContent(questionCell.html());
-						pollCell = nextContentRows.eq(1).find('td:not(:empty):has(td.comment)');
-					} else {
-						pollCell = nextContentRows.eq(0).find('td:not(:empty):has(td.comment)');
-					}
-					if (pollCell.length){
-						poll = [];
-						pollCell.find('td.comment').each(function(){
-							var el = $(this);
-							poll.push({
-								item: el.text().trim(),
-								points: parseInt(el.parent('tr').next('tr').find('.comhead span').text(), 10)
-							});
-						});
-					}
-					if (url.match(/^item/i)) type = 'ask';
-					if (!user){ // No users post this = job ads
-						type = 'job';
-						id = (url.match(/\d+/) || [])[0];
-						timeAgo = cell2.text().trim();
-					}
-				} else {
-					var cell = table1.find('td.default');
-					if (cell.length){
-						var userLink = cell.find('a[href^=user]');
-						user = userLink.text(),
-						timeAgo = userLink[0] ? userLink[0].nextSibling.textContent.replace('|', '').trim() : '',
-						id = (cell.find('a[href^=item]').attr('href').match(/\d+/) || [])[0],
-						content = cleanContent(table1.find('.comment').html());
-						type = 'comment';
-					}
-				}
-				var post = {
-						id: id,
-						title: title,
-						url: url,
-						domain: domain,
-						points: points,
-						user: user,
-						time_ago: timeAgo,
-						comments_count: commentsCount,
-						content: content,
-						poll: poll,
-						type: type,
-						comments: [],
-						more_comments_id: null
-					},
-					table2 = table1.nextAll('table:first');
-
-				// If there are comments for a post
-				if (table2.length){
-					var commentRows = table2.find('tr table');
-					post.comments = processComments(commentRows, $);
-
-					// Check for 'More' comments (Rare case)
-					var more = $('td.title a[href^="/x?"]');
-					if (more.length){
-						// Whatever 'fnid' means
-						var fnid = more.attr('href').match(/fnid=(\w+)/);
-						if (fnid){
-							fnid = fnid[1];
-							post.more_comments_id = fnid;
-						}
-					}
-				}
-
-				cache.set(cacheKey, post, CACHE_EXP);
-				res.jsonp(post);
+				var data = hndom.comments(body);
+				cache.set(cacheKey, data, CACHE_EXP);
+				res.jsonp(data);
 			});
 		}
 	});
@@ -497,31 +286,9 @@ app.get(/^\/comments\/(\w+)$/, function(req, res){
 					return;
 				}
 
-				var window = domino.createWindow(body);
-				window._run(jquery);
-				var $ = window.$;
-
-				var post = {
-						comments: [],
-						more_comments_id: null
-					};
-
-				var commentRows = $('table:not(:has(table)):has(.comment)');
-				post.comments = processComments(commentRows, $);
-
-				// Check for 'More' comments (Rare case)
-				var more = $('td.title a[href^="/x?"]');
-				if (more.length){
-					// Whatever 'fnid' means
-					var fnid = more.attr('href').match(/fnid=(\w+)/);
-					if (fnid){
-						fnid = fnid[1];
-						post.more_comments_id = fnid;
-					}
-				}
-
-				cache.set(cacheKey, post, CACHE_EXP);
-				res.jsonp(post);
+				var data = hndom.comments(body);
+				cache.set(cacheKey, data, CACHE_EXP);
+				res.jsonp(data);
 			});
 		}
 	});
@@ -539,16 +306,9 @@ app.get('/newcomments', function(req, res){
 					errorRespond(res, err);
 					return;
 				}
-				var window = domino.createWindow(body);
-				window._run(jquery);
-				var $ = window.$;
-
-				var comments = [],
-					commentRows = $('tr:nth-child(3) tr:has(span.comment)');
-				comments = processComments(commentRows, $);
-
-				cache.set(cacheKey, comments, CACHE_EXP);
-				res.jsonp(comments);
+				var data = hndom.newComments(body);
+				cache.set(cacheKey, body, CACHE_EXP);
+				res.jsonp(body);
 			});
 		}
 	});
@@ -567,27 +327,9 @@ app.get(/^\/user\/(\w+)$/, function(req, res){
 					errorRespond(res, err);
 					return;
 				}
-				var window = domino.createWindow(body);
-				window._run(jquery);
-				var $ = window.$;
-
-				var cells = $('form tr td:odd');
-				var id = cells.eq(0).text(),
-					created = cells.eq(1).text(),
-					karma = parseInt(cells.eq(2).text(), 10),
-					avg = parseFloat(cells.eq(3).text()),
-					about = cleanContent(cells.eq(4).html());
-
-				var user = {
-						id: id,
-						created: created,
-						karma: karma,
-						avg: avg,
-						about: about
-					};
-
-				cache.set(cacheKey, user, CACHE_EXP);
-				res.jsonp(user);
+				var data = hndom.user(body);
+				cache.set(cacheKey, data, CACHE_EXP);
+				res.jsonp(data);
 			});
 		}
 	});
